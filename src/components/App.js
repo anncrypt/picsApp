d9ee524a975f77c0c3b477a3c8196aa490a6f9f3
Anna Ionova <echoespond@gmail.com>
2019-01-09 15:22:52 -0500

    initial setup for imgs app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
