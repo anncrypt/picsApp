76b32751bc4ac3c136dea609b05e842a4ebfff53
Anna Ionova <echoespond@gmail.com>
2019-01-12 22:24:12 -0500

    final style changes to the app

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,18 +4,27 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
 
     this.imgRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imgRef.current.clientHeight);
+    this.imgRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imgRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   }
 
   render() {
     const { description, urls} = this.props.image;
     return(
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img 
           ref={this.imgRef}
           src={urls.regular} 
